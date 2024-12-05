5236d137eac493d6006eda716ea5a61c15857051
Jakob Heuser <jakob@codedrift.com>
2024-12-04 17:28:42 -0800

    feat: Adds standard JS infra

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-# Extism TS PDK Plugin
+# Taskless Core Plugin
 
-See more documentation at https://github.com/extism/js-pdk and
-[join us on Discord](https://extism.org/discord) for more help.
+The Taskless Core plugin powers the Open Source [Taskless Loader](https://github.com/taskless/loader-js) and is the default pack most users use when starting with Taskless.
+
+# What Are Packs?
+
+Packs are WebAssembly binaries that expose one or more "hooks" to the host system. In the case of Taskless, these WebAssembly hooks must expose a `pre` and `post` method which operate on strings containing JSON.
+
+# Building Locally
+
+Taskless Packs are built as Extism plugins. This one is (currently) TypeScript based, and requires the Extism PDK to build.
+
+1. Follow the `js-pdk` instructions to install Extism's builder for JavaScript and TypeScript projects ([link](https://github.com/extism/js-pdk))
+2. Run `pnpm install` to install the development dependnecies
+3. Run `pnpm build` to build the plugin
+
+In the `dist` folder, you'll see the final JavaScript, along with a `plugin.wasm` file (the WebAssembly binary) and a `manifest.json` file that describes the plugin.
+
+# Testing
+
+This library uses [@taskless/packcheck](https://github.com/taskless/packcheck) to test the WebAssembly binary. To run the tests, run `pnpm test`.
--- packbuild.js
+++ packbuild.js
@@ -1,29 +1,15 @@
-import YAML from 'yaml'
-import { readFileSync, writeFileSync } from "fs";
-import { resolve } from "path";
-import { uint8ArrayToBase64 } from "uint8array-extras"
+import { writeFileSync } from "node:fs";
+import path from "node:path";
 
 const __dirname = new URL(".", import.meta.url).pathname;
 
 const template = {
-  schema: 3,
+  schema: "pre1",
   name: "core",
-  version: "1.0.0",
+  version: "0.0.1",
   description:
     "Taskless core Telemetry. The core telemetry contains common monitoring and logging found in APM-like solutions, and is a solid baseline for any observability stack.",
   capture: {
-    durationMs: {
-      type: "number",
-      description: "The duration of the request in milliseconds",
-    },
-    status: {
-      type: "number",
-      description: "The status code of the request",
-    },
-    domain: {
-      type: "string",
-      description: "The domain of the request",
-    },
     url: {
       type: "string",
       description: "The full URL of the request",
@@ -34,21 +20,28 @@ const template = {
     },
     error: {
       type: "string",
-      description: "The error message",
+      description: "The error message in the event of a non-200 response code",
+    },
+    domain: {
+      type: "string",
+      description: "The domain of the request",
+    },
+    status: {
+      type: "number",
+      description: "The status code of the request",
+    },
+    durationMs: {
+      type: "number",
+      description: "The duration of the request in milliseconds",
     },
   },
   permissions: {
     domains: [".+"],
     response: ["body", "headers"],
-  }
-}
-
-const wasm = readFileSync(resolve(__dirname, "./dist/plugin.wasm"));
-
-// convert the wasm string to base64
-template.module = uint8ArrayToBase64(wasm);
+  },
+};
 
 writeFileSync(
-  resolve(__dirname, "./dist/taskless-oss.yaml"),
-  YAML.stringify(template)
+  path.resolve(__dirname, "./dist/manifest.json"),
+  JSON.stringify(template, null, 2)
 );
